78cecbb31e4fa21cb6b3178847bf9edf2319fe34
alexErm0 <ermolaevaleks26@gmail.com>
2026-02-24 18:35:00 +0100

    fixing express error

--- server/server.js
+++ server/server.js
@@ -88,11 +88,11 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("Server running on port 5000");
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
